9e483719ebb323d3e9d4f5638173a8ffca4da31d
Gheorghe Pinzaru <feross95@gmail.com>
2020-08-26 16:25:23 +0300

    Handle wallet signals

--- services/local-notifications/core.go
+++ services/local-notifications/core.go
@@ -4,11 +4,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math/big"
+
+	"github.com/status-im/status-go/signal"
 
 	messagebus "github.com/vardius/message-bus"
+
+	"github.com/ethereum/go-ethereum/common"
 )
 
-type messagePayload []byte
+type messagePayload interface{}
 
 // MessageType - Enum defining types of message handled by the bus
 type MessageType string
@@ -30,6 +35,15 @@ type Notification struct {
 	Image         string           `json:"imageUrl,omitempty"`
 	IsScheduled   bool             `json:"isScheduled,omitempty"`
 	ScheduledTime string           `json:"scheduleTime,omitempty"`
+}
+
+// TransactionEvent - structure used to pass messages from wallet to bus
+type TransactionEvent struct {
+	Type                      string
+	BlockNumber               *big.Int
+	Accounts                  []common.Address
+	NewTransactionsPerAccount map[common.Address]int
+	ERC20                     bool
 }
 
 const (
@@ -63,31 +77,25 @@ func pushMessage(notification *Notification) {
 	// Send signal with this notification
 	jsonBody, _ := json.Marshal(notification)
 	fmt.Println(string(jsonBody))
+
+	signal.SendLocalNotifications(notification)
 }
 
 func buildTransactionNotification(payload messagePayload) *Notification {
-
-	transaction := struct {
-		ID    string `json:"id"`
-		State string `json:"state"`
-		From  string `json:"from"`
-		To    string `json:"to"`
-		Value string `json:"value"`
-	}{}
-
-	if err := json.Unmarshal(payload, &transaction); err != nil {
-		return nil
+	transaction := payload.(TransactionEvent)
+	body := notificationBody{
+		State: transaction.Type,
+		From:  transaction.Type,
+		To:    transaction.Type,
+		Value: transaction.Type,
 	}
 
-	body := notificationBody{
-		State: transaction.State,
-		From:  transaction.From,
-		To:    transaction.To,
-		Value: transaction.Value,
+	for i, s := range transaction.Accounts {
+		fmt.Println(i, s)
 	}
 
 	return &Notification{
-		ID:       transaction.ID,
+		ID:       string(transaction.Type),
 		Body:     body,
 		Category: "transaction",
 	}
@@ -102,6 +110,8 @@ func TransactionsHandler(ctx context.Context, payload messagePayload) {
 // NotificationWorker - Worker which processes all incoming messages
 func (s *Broker) NotificationWorker() error {
 
+	fmt.Println("NotificationWorker")
+
 	if err := s.bus.Subscribe(string(Transaction), TransactionsHandler); err != nil {
 		return err
 	}
@@ -110,6 +120,7 @@ func (s *Broker) NotificationWorker() error {
 }
 
 // PublishMessage - Send new message to be processed into a notification
-func (s *Broker) PublishMessage(messageType MessageType, payload string) {
+func (s *Broker) PublishMessage(messageType MessageType, payload interface{}) {
 	s.bus.Publish(string(messageType), s.ctx, messagePayload(payload))
+	fmt.Println("Messag published", string(messageType))
 }
